976070ef2119c85e812d16afe8e93ee39eee7a2e
Apple <>
2023-06-02 18:14:51 +0000

    Update Readme links.

--- Swift/AVCam/ItemSelectionViewController.swift
+++ Swift/AVCam/ItemSelectionViewController.swift
@@ -1,5 +1,5 @@
 /*
-See LICENSE folder for this sample’s licensing information.
+See the LICENSE.txt file for this sample’s licensing information.
 
 Abstract:
 The view controller that presents the SSM selection interface.
